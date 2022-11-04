d6db13464dc368c8c73c6189ea711ccd15821280
Parwiz-Haqbien <Prwiz.jan03@gmail.com>
2022-11-04 16:21:18 +1100

    adding the seeds

--- Main/db/seeds.sql
+++ Main/db/seeds.sql
@@ -1,46 +1,25 @@
---department seeds--
+
 INSERT INTO department (names)
 VALUES ('Fianance'),
-
-INSERT INTO department (names)
-VALUES ('Legal'),
-
-INSERT INTO department (names)
-VALUES ('Engineering'),
-
-INSERT INTO department (names)
-VALUES ('Sales'),
+('legal'), 
+('Engineering'),
+('sales');
 
 -- Employees role seeds--
 INSERT INTO roles (title, salary, department_id)
-VALUES ('Accountant','80,000', 1),
+VALUES ('Accountant',80000, 1),
+('Lead accountant',75000, 3),
+('sales',55000, 2),
+('sales assistance',45000, 5);
 
-INSERT INTO roles (title, salary, department_id)
-VALUES ('Lead accountant','75,000', 3),
-
-INSERT INTO roles (title, salary, department_id)
-VALUES ('sales','55,000', 2),
-
-INSERT INTO roles (title, salary, department_id)
-VALUES ('sales assistance','45,000', 5),
 
 -- Employee seeds
 INSERT INTO employee (first_name, last_name , roles_id , manager_id)
 VALUES ('John' , 'Merry' , 2 , 4),
+('Leo' , 'Messi' , 1 , 3),
+('Harry' , 'Maguire' , 3 , 5),
+('Ali' , 'Mohammad' , 4 , 1),
+('Hamza' , 'Joyndia' , 5 , 2);
 
-INSERT INTO employee (first_name, last_name , roles_id , manager_id)
-VALUES ('Leo' , 'Messi' , 1 , 3),
 
-INSERT INTO employee (first_name, last_name , roles_id , manager_id)
-VALUES ('Harry' , 'Maguire' , 3 , 5),
 
-INSERT INTO employee (first_name, last_name , roles_id , manager_id)
-VALUES ('Ali' , 'Mohammad' , 4 , 1),
-
-INSERT INTO employee (first_name, last_name , roles_id , manager_id)
-VALUES ('Hamza' , 'Joyndia' , 5 , 2),
-
--- selecting all for creating tables
-SELECT * FROM department;
-SELECT * FROM roles;
-SELECT * FROM employee; 
--- server.js
+++ server.js
@@ -12,3 +12,161 @@ const db = mysql.createConnection (
         database: 'tracker_db' 
     }, console.log('This is connected to the tracker_db database ')
 );
+
+db.connect(function(err) {
+    if (err) throw err;
+    startPrompt();
+  });
+
+  function startPrompt() {
+    inquirer
+      .prompt([ 
+        {
+        name: "choice",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update employee role",
+          "Exit"
+        ]
+    }
+      
+    ]).then(function(answser) {
+        switch(answser.choice) {
+       case "View all deparment?": 
+       viewALLDepartment();
+       break;
+       
+       case "View all roles?": 
+       viewALLRoles();
+       break;
+
+       case "View all employees?": 
+       viewALLEmployees();
+       break;
+
+       case "add a department?": 
+       addDepartment();
+       break;
+
+       case "add a role?": 
+       addRole();
+       break;
+
+       case "add a employee?": 
+       addEmployee();
+       break;
+
+       case "update a employee role?": 
+       updateEmployee();
+       break;
+
+       case "would you like to exist?": 
+       exitOut();
+       break; 
+        }
+    })
+  }
+
+  function viewALLDepartment() {
+    db.query("SELECT employee.first_name, employee.last_name, deparment.name As Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role;",
+    function(error, respond) {
+        if(error) throw error
+        console.table(respond)
+        startPrompt()
+    })
+  }
+
+  function viewALLRoles() {
+    db.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;",
+    function(error, respond) {
+        if(error) throw error
+        console.table(respond)
+        startPrompt()
+    })
+  }
+  
+  function viewALLEmployees() {
+    db.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name,'',e.last_name) AS Manger FROM employee;",
+    function(error, respond) {
+        if(error) throw error
+        console.table(respond)
+        startPrompt()
+    })
+}
+
+   var roleAdding = [];
+   function selectRole() {
+    db.query('SELECT + FROM role', function(error, respond) {
+        if(error) throw error
+        for(var i = 0; i < respond.length; ++) {
+            roleAdding.push(respond[i].title)
+        }
+    })
+    return roleAdding
+   }
+
+   
+   var managerAdding = [];
+   function selectManager() {
+    db.query('SELECT first_name, last_name FROM employee WHERE manager_id IS NULL', function(error, respond) {
+        if(error) throw error
+        for(var i = 0; i < respond.length; ++) {
+            managerAdding.push(respond[i].title)
+        }
+    })
+    return managerAdding
+   }
+   
+   function addEmployee() {
+    inquirer
+    .prompt([
+        {
+            name: "firstname",
+            type: "input",
+            message: "What is their first name?"
+        },
+
+        {
+            name: "lastname",
+            type: "input",
+            message: "What is their last name?"
+        },
+
+        {
+            name: "role",
+            type: "input",
+            message: "What is their role?",
+            choices: selectRole()
+        },
+
+        {
+            name: "option",
+            type: "rawlist",
+            message: "What is their manager name?",
+            choices: selectManager()
+        },
+
+    ]).then(function(answser) {
+        var roleId = selectRole().indexOf(answser.role) + 1 
+        var managerId = selectManager().indexOf(answser.choice) + 1 
+        db.query('Add to employee?', 
+        {
+            first_name: answser.first_name,
+            last_name: answser.last_name,
+            role_id: answser.roleId,
+            manager_id: answser.managerId,
+
+             }, function(error) {
+                    if (error) throw error;
+                    console.table(answser)
+                    startPrompt();
+             })
+    })
+   }
